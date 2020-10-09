c81dcae29ae0c0ce68aa55e4f7db4c1af79834d6
sala19a <samuelbatistar99@gmail.com>
2020-10-08 21:47:47 -0300

    ta maneiro

--- app/src/main/java/com/projetopadrao/MainActivity.java
+++ app/src/main/java/com/projetopadrao/MainActivity.java
@@ -3,19 +3,53 @@ package com.projetopadrao;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.login);
 
 
 
+        Log.d("ciclo_de_vida", "onCreate - a atividade iniciou");
 
 
 
 
     }
+
+    @Override
+    protected void onStart(){
+        Log.d("ciclo_de_vida", "onStart - o codigo da atividade começou a ser feito");
+        super.onStart();
+
+    }
+
+    @Override
+    protected void  onResume(){
+        Log.d("ciclo_de_vida", "onResume - Estado de interação com tela");
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause(){
+        Log.d("ciclo_de_vida", "onPause - iniciou o término da activity");
+        super.onPause();
+    }
+
+    @Override
+    protected void onStop(){
+        Log.d("ciclo_de_vida", "onStop - A atividade não está mais visivel ao usuario");
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy(){
+        Log.d("ciclo_de_vida", "onDestroy - A Activity foi completamente destruida");
+        super.onDestroy();
+    }
 }
+
